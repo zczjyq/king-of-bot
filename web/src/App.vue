2769f28b431943e7bd862d0b0aabbd1f420757dc
嘉麒 蔡 <937473303@qq.com>
2024-02-24 01:06:07 +0800

    后端前端通信

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,32 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>Bot昵称:{{ bot_name }}</div>
+  <div>Bot战力: {{ bot_rating }}</div>
+  <router-view></router-view>
 </template>
 
+<script>
+import { ref } from "vue";
+import $ from "jquery";
+export default {
+  name: "App",
+  setup: () => {
+    let bot_name = ref("");
+    let bot_rating = ref("");
+    $.ajax({
+      url: "http://localhost:3000/pk/getbotinfo/",
+      type: "get",
+      success: (resp) => {
+        bot_name.value = resp.name;
+        bot_rating.value = resp.rating;
+      },
+    });
+    return {
+      bot_name,
+      bot_rating,
+    };
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
