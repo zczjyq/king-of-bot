42ad748cac1484775849e13ce208d77432b2481c
嘉麒 蔡 <937473303@qq.com>
2024-02-24 23:32:46 +0800

    前端地图的创建

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,5 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import NotFound from '../views/error/NotFound.vue'
+import PkIndexView from '../views/pk/PkIndexView.vue'
+import RanklistIndexView from '../views/ranklist/RanklistIndexView.vue'
+import RecordIndexView from '../views/record/RecordIndexView.vue'
+import UserBotIndexView from '../views/user/bot/UserBotIndexView.vue'
+
 const routes = [
+  {
+    path: '/',
+    name: 'home',
+    redirect: "/pk/"
+  },
+  {
+    path: "/pk/",
+    name: "pk_index",
+    component: PkIndexView
+  },
+  {
+    path: "/record/",
+    name: "record_index",
+    component: RecordIndexView
+  },
+  {
+    path: "/ranklist/",
+    name: "ranklist_index",
+    component: RanklistIndexView
+  },
+  {
+    path: "/user/bot/",
+    name: "user_bot_index",
+    component: UserBotIndexView
+  },
+  {
+    path: "/404/",
+    name: "not_found_index",
+    component: NotFound
+  },
+  {
+    path: "/:catchAll(.*)*",
+    redirect: "/404/"
+  }
 ]
 
 const router = createRouter({
---
+++ web/src/views/record/RecordIndexView.vue
@@ -0,0 +1,15 @@
+<template>
+  <ContentField>玩家列表</ContentField>
+</template>
+
+<script>
+import ContentField from "../../components/ContentField.vue";
+export default {
+  components: {
+    ContentField,
+  },
+};
+</script>
+
+<style scoped>
+</style>
