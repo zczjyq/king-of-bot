17d8621fad3091934795f60f9e607a81610aa294
嘉麒 蔡 <937473303@qq.com>
2024-02-29 03:00:12 +0800

    实现微服务：匹配系统 同步了三个棋盘 在数据库中创建表record

--- backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
+++ backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
@@ -3,6 +3,7 @@ package com.kob.backend.consumer;
 import com.alibaba.fastjson2.JSONObject;
 import com.kob.backend.consumer.utils.Game;
 import com.kob.backend.consumer.utils.JwtAuthentication;
+import com.kob.backend.mapper.RecordMapper;
 import com.kob.backend.mapper.UserMapper;
 import com.kob.backend.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,6 @@ import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.util.Iterator;
-import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArraySet;
 
@@ -21,18 +21,25 @@ import java.util.concurrent.CopyOnWriteArraySet;
 @ServerEndpoint("/websocket/{token}")  // 注意不要以'/'结尾
 public class WebSocketServer {
 
-    final static ConcurrentHashMap<Integer, WebSocketServer>
+    final public static ConcurrentHashMap<Integer, WebSocketServer>
             users = new ConcurrentHashMap<>();
-    final static CopyOnWriteArraySet<User> matchpool = new CopyOnWriteArraySet<>();
+    final private static CopyOnWriteArraySet<User> matchpool = new CopyOnWriteArraySet<>();
     private User user;
     private Session session = null;
+    private Game game = null;
 
     private static UserMapper userMapper;
+    public static RecordMapper recordMapper;
 
     @Autowired
     public void setUserMapper(UserMapper userMapper) {
         WebSocketServer.userMapper = userMapper;
     }
+
+   @Autowired
+   public void setRecordMapper(RecordMapper recordMapper) {
+        WebSocketServer.recordMapper = recordMapper;
+   }
 
     // 当有新连接建立时调用此方法
     @OnOpen
@@ -60,32 +67,54 @@ public class WebSocketServer {
             matchpool.remove(this.user);
         }
     }
-
+    /**
+     * 开始匹配对战玩家，并创建游戏实例进行对战
+     */
     private void startMatching() {
         System.out.println("start-matching");
         matchpool.add(this.user);
 
+        // 当匹配池中有足够的玩家时，开始匹配对战
         while (matchpool.size() >= 2) {
+            // 从匹配池中取出两个玩家进行对战
             Iterator<User> it = matchpool.iterator();
             User a = it.next(), b = it.next();
             matchpool.remove(a);
             matchpool.remove(b);
 
-            Game game = new Game(15, 16, 40);
+            // 创建游戏实例并生成地图
+            Game game = new Game(15, 16, 40, a.getId(), b.getId());
             game.createMap();
 
+            // 将玩家与游戏实例关联
+            users.get(a.getId()).game = game;
+            users.get(b.getId()).game = game;
+            game.start();
+
+            // 准备发送给玩家的游戏信息
+            JSONObject respGame = new JSONObject();
+            respGame.put("a_id", game.getPlayerA().getId());
+            respGame.put("a_sx", game.getPlayerA().getSx());
+            respGame.put("a_sy", game.getPlayerA().getSy());
+            respGame.put("b_id", game.getPlayerB().getId());
+            respGame.put("b_sx", game.getPlayerB().getSx());
+            respGame.put("b_sy", game.getPlayerB().getSy());
+            respGame.put("map", game.getG());
+
+            // 准备发送给玩家A的消息
             JSONObject respA = new JSONObject();
             respA.put("event", "start-matching");
             respA.put("opponent_username",b.getUsername());
             respA.put("opponent_photo", b.getPhoto());
-            respA.put("game_map", game.getG());
+            respA.put("game", respGame);
             users.get(a.getId()).sendMessage(respA.toJSONString());
 
+            // 准备发送给玩家B的消息
             JSONObject respB = new JSONObject();
             respB.put("event", "start-matching");
             respB.put("opponent_username",a.getUsername());
             respB.put("opponent_photo", a.getPhoto());
-            respB.put("game_map", game.getG());
+            respB.put("game", respGame);
             users.get(b.getId()).sendMessage(respB.toJSONString());
         }
     }
@@ -93,6 +122,14 @@ public class WebSocketServer {
     private void stopMatching() {
         System.out.println("stop-matching");
         matchpool.remove(this.user);
+    }
+
+    private void move(int direction) {
+        if (game.getPlayerA().getId().equals(user.getId())) {
+            game.setNextstepA(direction);
+        } else if (game.getPlayerB().getId().equals(user.getId())) {
+            game.setNextstepB(direction);
+        }
     }
 
     // 当接收到客户端消息时调用此方法
@@ -104,17 +141,23 @@ public class WebSocketServer {
         String event = data.getString("event");
         if ("start-matching".equals(event)) {
             startMatching();
-        } else {
+        } else if ("stop-matching".equals(event)) {
             stopMatching();
+        } else if ("move".equals(event)) {
+            move(data.getInteger("direction"));
         }
     }
 
     // 当发生错误时调用此方法
     @OnError
-    public void onError(Session  session, Throwable error) {
+    public void onError(Session session, Throwable error) {
         error.printStackTrace();
     }
 
+    /**
+     * 向 WebSocket 会话发送消息
+     * @param message 要发送的消息内容
+     */
     public void sendMessage(String message) {
         synchronized (this.session) {
             try {
--- web/src/store/pk.js
+++ web/src/store/pk.js
@@ -5,12 +5,25 @@ export default {
     opponent_username: "",
     opponent_photo: "",
     gamemap: null,
+    a_id: 0,
+    a_sx: 0,
+    a_sy: 0,
+    b_id: 0,
+    b_sx: 0,
+    b_sy: 0,
+    gameObject: null,
+    loser: "none",// all, A, B
   },
   getters: {
   },
   mutations: {
     updateSocket(state, socket) {
       state.socket = socket;
+    },
+
+    emptyOpponent(state) {
+      state.opponent_username = "我的对手";
+      state.opponent_photo = "https://cdn.acwing.com/media/article/image/2022/08/09/1_1db2488f17-anonymous.png";
     },
     updateOpponent(state, oppponent) {
       state.opponent_username = oppponent.username;
@@ -21,8 +34,22 @@ export default {
       state.status = status;
     },
 
-    updateGameMap(state, gamemap) {
-      state.gamemap = gamemap;
+    updateGame(state, game) {
+      state.gamemap = game.map;
+      state.a_id = game.a_id;
+      state.a_sx = game.a_sx;
+      state.a_sy = game.a_sy;
+      state.b_id = game.b_id;
+      state.b_sx = game.b_sx;
+      state.b_sy = game.b_sy;
+    },
+
+    updateGameObject(state, gameObject) {
+      state.gameObject = gameObject;
+    },
+
+    updateLoser(state, loser) {
+      state.loser = loser;
     }
   },
   actions: {
