7d3b45c6639e542a7cd08f9bd463a1d40613db57
嘉麒 蔡 <937473303@qq.com>
2024-02-29 18:15:16 +0800

    匹配系统完成

---
+++ backencloud/backend/src/main/java/com/kob/backend/consumer/utils/Game.java
@@ -0,0 +1,266 @@
+package com.kob.backend.consumer.utils;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.kob.backend.consumer.WebSocketServer;
+import com.kob.backend.pojo.Record;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Game extends Thread{
+    private final Integer rows;
+    private final Integer cols;
+    private final Integer inner_walls_count;
+    @Getter
+    private final int[][] g;
+    private final static int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1};
+
+    @Getter
+    private final Player playerA, playerB;
+    private Integer nextstepA = null;
+    private Integer nextstepB = null;
+    private ReentrantLock lock = new ReentrantLock();
+    private String status = "playing";
+    private String loser = "";
+
+    public Game(Integer rows, Integer cols, Integer inner_walls_count, Integer idA, Integer idB) {
+        this.rows = rows;
+        this.cols = cols;
+        this.inner_walls_count = inner_walls_count;
+        this.g = new int[rows][cols];
+        playerA = new Player(idA, rows - 2, 1, new ArrayList<>());
+        playerB = new Player(idB, 1, cols - 2, new ArrayList<>());
+    }
+
+    private boolean check_connectivity(int sx, int sy, int tx, int ty) {
+        if (sx == tx && sy == ty) return true;
+        g[sx][sy] = 1;
+
+        for (int i = 0; i < 4; i ++ ) {
+            int x = sx + dx[i], y = sy + dy[i];
+            if (x >= 0 && x < this.rows && y >= 0 && y < this.cols && g[x][y] == 0) {
+                if (check_connectivity(x, y, tx, ty)) {
+                    g[sx][sy] = 0;
+                    return true;
+                }
+            }
+        }
+
+        g[sx][sy] = 0;
+        return false;
+    }
+
+    private boolean draw() {  // 画地图
+        for (int i = 0; i < this.rows; i ++ ) {
+            for (int j = 0; j < this.cols; j ++ ) {
+                g[i][j] = 0;
+            }
+        }
+
+        for (int r = 0; r < this.rows; r ++ ) {
+            g[r][0] = g[r][this.cols - 1] = 1;
+        }
+        for (int c = 0; c < this.cols; c ++ ) {
+            g[0][c] = g[this.rows - 1][c] = 1;
+        }
+
+        Random random = new Random();
+        for (int i = 0; i < this.inner_walls_count / 2; i ++ ) {
+            for (int j = 0; j < 1000; j ++ ) {
+                int r = random.nextInt(this.rows);
+                int c = random.nextInt(this.cols);
+
+                if (g[r][c] == 1 || g[this.rows - 1 - r][this.cols - 1 - c] == 1)
+                    continue;
+                if (r == this.rows - 2 && c == 1 || r == 1 && c == this.cols - 2)
+                    continue;
+
+                g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = 1;
+                break;
+            }
+        }
+
+        return check_connectivity(this.rows - 2, 1, 1, this.cols - 2);
+    }
+
+    public void createMap() {
+        for (int i = 0; i < 1000; i ++ ) {
+            if (draw())
+                break;
+        }
+    }
+
+    public void setNextstepA(Integer nextstepA) {
+        lock.lock();
+        try {
+            this.nextstepA = nextstepA;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void setNextstepB(Integer nextstepB) {
+        lock.lock();
+        try {
+            this.nextstepB = nextstepB;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private boolean nextStep() {
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (int i = 0; i < 50; i  ++ ) {
+            try {
+                Thread.sleep(100);
+                lock.lock();
+                try {
+                    if (nextstepA!=null && nextstepB != null) {
+                        playerA.getSteps().add(nextstepA);
+                        playerB.getSteps().add(nextstepB);
+                        return true;
+                    }
+                } finally {
+                    lock.unlock();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    private boolean check_valid(List<Cell> cellsA, List<Cell> cellsB) {
+        int n = cellsA.size();
+        Cell cell = cellsA.get(n - 1);
+        if (g[cell.x][cell.y] == 1) return false;
+
+        for (int i = 0; i < n - 1; i ++ ) {
+            if (cellsA.get(i).x == cell.x && cellsA.get(i).y == cell.y)
+                return false;
+        }
+        for (int i = 0; i < n - 1; i ++ ) {
+            if (cellsB.get(i).x == cell.x && cellsB.get(i).y == cell.y)
+                return false;
+        }
+
+        return true;
+    }
+
+    private void judge() { //判断两名玩家操作是否合法
+        List<Cell> cellsA = playerA.getCells();
+        List<Cell> cellsB = playerB.getCells();
+
+        boolean validA = check_valid(cellsA, cellsB);
+        boolean validB = check_valid(cellsB, cellsA);
+        if (!validA|| !validB) {
+            status = "finished";
+
+            if (!validA && !validB) {
+                loser = "all";
+            } else if (!validA) {
+                loser = "A";
+            } else if (!validB) {
+                loser = "B";
+            }
+        }
+    }
+
+    private void sendAllMessage(String message) {
+        if (WebSocketServer.users.get(playerA.getId()) != null)
+            WebSocketServer.users.get(playerA.getId()).sendMessage(message);
+        if (WebSocketServer.users.get(playerB.getId()) != null)
+            WebSocketServer.users.get(playerB.getId()).sendMessage(message);
+    }
+
+    private void sendMove() {
+        lock.lock();
+        try {
+            JSONObject resp = new JSONObject();
+            resp.put("event", "move");
+            resp.put("a_direction", nextstepA);
+            resp.put("b_direction", nextstepB);
+            sendAllMessage(resp.toString());
+            nextstepA = nextstepB = null;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private String getMapString() {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < rows; i ++ ) {
+            for (int j = 0; j < cols; j ++ ) {
+                res.append(g[i][j]);
+            }
+        }
+        return res.toString();
+    }
+
+    private void saveToDatabase() {
+        Record record = new Record(
+                null,
+                playerA.getId(),
+                playerA.getSx(),
+                playerA.getSy(),
+                playerB.getId(),
+                playerB.getSx(),
+                playerB.getSy(),
+                playerA.getstepString(),
+                playerB.getstepString(),
+                getMapString(),
+                loser,
+                new Date()
+        );
+
+        WebSocketServer.recordMapper.insert(record);
+    }
+
+    private void sendResult() { // 向两个client发结果
+        JSONObject resp = new JSONObject();
+        resp.put("event", "result");
+        resp.put("loser", loser);
+        saveToDatabase();
+        sendAllMessage(resp.toJSONString());
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i ++ ) {
+            if (nextStep()) {
+                judge();
+                if (status.equals("playing")) {
+                    sendMove();
+                } else {
+                    sendResult();
+                    break;
+                }
+            } else {
+                status = "finished";
+                lock.lock();
+                try {
+                    if (nextstepA == null && nextstepB == null) {
+                        loser = "all";
+                    } else if (nextstepA == null) {
+                        loser = "A";
+                    } else if (nextstepB == null){
+                        loser = "B";
+                    }
+                } finally {
+                    lock.unlock();
+                }
+                sendResult();
+                break;
+            }
+        }
+    }
+}
---
+++ backencloud/matchingsystem/src/main/java/com/kob/matchingsystem/service/impl/utils/Player.java
@@ -0,0 +1,16 @@
+package com.kob.matchingsystem.service.impl.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.annotation.sql.DataSourceDefinition;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Player {
+    private Integer userId;
+    private Integer rating;
+    private Integer waitingTime;
+}
