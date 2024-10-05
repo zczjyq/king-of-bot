6c1d4a9d463401ab87eb55f21ba4c33e546d51da
嘉麒 蔡 <937473303@qq.com>
2024-10-05 23:00:03 +0800

    加入战队功能完成

---
+++ backencloud/backend/src/main/java/com/kob/backend/controller/team/TeamJoinController.java
@@ -0,0 +1,47 @@
+package com.kob.backend.controller.team;
+
+import com.kob.backend.service.team.TeamJoinService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.kob.backend.constant.TeamConstant.*;
+import static com.kob.backend.constant.UtilMessage.STATICRETURNMESSAGE;
+
+@RestController
+public class TeamJoinController {
+    @Autowired
+    private TeamJoinService teamJoinService;
+    @PostMapping("/api/team/join/")
+    Map<String, String> join(@RequestParam Map<String, String> data) {
+        String userId = data.get("userId");
+        String teamId = data.get("teamId");
+        Map<String, String> map = new HashMap<>();
+
+        Integer userState = teamJoinService.check(data);
+        if (userState.equals(TEAM_JOIN_HAS_TEAM)) {
+            map.put(STATICRETURNMESSAGE, TEAM_JOIN_HAS_TEAM_MEG);
+            return map;
+        }
+        if (userState.equals(TEAM_JOIN_HAS_TEAM)) {
+            map.put(STATICRETURNMESSAGE, TEAM_JOIN_HAS_TEAM_MEG);
+            return map;
+        }
+        if (userState.equals(TEAM_JOIN_HAS_JOIN_THIS_TEAM)) {
+            map.put(STATICRETURNMESSAGE, TEAM_JOIN_HAS_JOIN_THIS_TEAM_MEG);
+            return map;
+        }
+        if (userState.equals(TEAM_JOIN_BANNED_TEAM)) {
+            map.put(STATICRETURNMESSAGE, TEAM_JOIN_BANNED_TEAM_MEG);
+            return map;
+        }
+
+        teamJoinService.join(data);
+        map.put(STATICRETURNMESSAGE, "success");
+        return map;
+    }
+}
